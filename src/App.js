43fd1f14e6d2c302d88fde0e0fd08f96fdcd4e55
Ismael Soares Doria <ismaelsoaresdoria@gmail.com>
2023-08-24 23:30:23 -0300

    Add structure react-to-do

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { useState, useEffect } from 'react';
+import { BsTrash, BsBookmarkCheck, BsBookmarkCheFill } from 'react-icons/bs';
+
+const API = "http://localhost:5000"
 function App() {
+  const [title, setTitle] = useState("");
+  const [time, setTime] = useState("");
+  const [todos, setTodos] = useState([])
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = e => {
+    e.PreventDefault();
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="todo-header">
+        <h1>React todo</h1>
+      </div>
+      <div className="form-todo">
+        <h2>Insira a sua próxima tarefa</h2>
+        <form onSubmit={handleSubmit}>
+
+        </form>
+      </div>
+      <div className="list-todo">
+        <p>Lista</p>
+        <h2>Lista de Tarefas</h2>
+        {todos.length === 0 && <p>Não há tarefas</p>}
+      </div>
     </div>
   );
 }
